2c4cd20f247af84e538b9d20b1bf111f17ac9a6e
harshil8705 <harshil8705@users.noreply.github.com>
2025-10-30 00:13:11 +0530

    Updated GraphQL Schema

--- src/main/java/com/graphql/gateway/repository/DepartmentRepository.java
+++ src/main/java/com/graphql/gateway/repository/DepartmentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository extends JpaRepository<Long, Department> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
